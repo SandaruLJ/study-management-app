bd0f429124196f2030b87cde05a600c63d15fd86
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-09-16 17:18:18 +0530

    Design and implement add subject fragment

--- app/src/main/java/com/example/timetable/Subject/AddSubjectFragment.java
+++ app/src/main/java/com/example/timetable/Subject/AddSubjectFragment.java
@@ -1,14 +1,31 @@
 package com.example.timetable.Subject;
 
+import android.content.res.ColorStateList;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.timetable.ColorPicker;
+import com.example.timetable.Course.DisplayCourseFragment;
+import com.example.timetable.Database.DBHandler;
 import com.example.timetable.R;
+import com.example.timetable.SelectDateFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +34,69 @@ import com.example.timetable.R;
  */
 public class AddSubjectFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AddSubjectFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddSubjectFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddSubjectFragment newInstance(String param1, String param2) {
-        AddSubjectFragment fragment = new AddSubjectFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    EditText subjectName, teacherName, subjectDesc;
+    ColorStateList col;
+    DBHandler db;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        db = new DBHandler(getActivity().getApplicationContext());
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_subject, container, false);
+
+        Button btn = view.findViewById(R.id.add_subject);
+        subjectName = (EditText) view.findViewById(R.id.subject_name);
+        teacherName = (EditText) view.findViewById(R.id.teacher_name);
+        subjectDesc = (EditText) view.findViewById(R.id.subject_desc);
+
+        //Colour Picker
+        final Button colorbtn = (Button) view.findViewById(R.id.colorbtn);
+        final Button bgBtn = (Button) view.findViewById(R.id.testbtn);
+        colorbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                ColorPicker cp = new ColorPicker();
+                cp.openColorPicker(getChildFragmentManager(), colorbtn, bgBtn);
+            }
+
+        });
+
+        ImageView allBtn = view.findViewById(R.id.all_subjects_btn);
+
+        allBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllSubjectsFragment fragment = new AllSubjectsFragment();
+                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(null).commit();
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean isInserted = db.addSubject(subjectName.getText().toString(), teacherName.getText().toString(), subjectDesc.getText().toString(), ((ColorDrawable) bgBtn.getBackground()).getColor());
+
+                if (isInserted) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Subject Added Successfully", Toast.LENGTH_LONG).show();
+                    AllSubjectsFragment fragment = new AllSubjectsFragment();
+                    AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(null).commit();
+                } else
+                    Toast.makeText(getActivity().getApplicationContext(), "Insert failed, Try again", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_subject, container, false);
+        return view;
     }
 }
