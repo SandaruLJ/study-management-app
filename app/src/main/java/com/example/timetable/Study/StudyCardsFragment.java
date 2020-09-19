150b6bc42df01715ab2161cf2027c5ba9b2b71a7
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-09-19 15:30:37 +0530

    Design and implement all studies fragment

---
+++ app/src/main/java/com/example/timetable/Study/StudyCardsFragment.java
@@ -0,0 +1,71 @@
+package com.example.timetable.Study;
+
+import android.database.Cursor;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.timetable.Database.DBHandler;
+import com.example.timetable.R;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StudyCardsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StudyCardsFragment extends Fragment {
+    private DBHandler db;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = new DBHandler(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_study_cards, container, false);
+
+        final ArrayList<Integer> studyIds = new ArrayList<>();
+        final ArrayList<String> studyTitles = new ArrayList<>();
+        final ArrayList<String> subjectNames = new ArrayList<>();
+        final ArrayList<Integer> studyColours = new ArrayList<>();
+        final ArrayList<String> studyDates = new ArrayList<>();
+        final ArrayList<String> studyStartTimes = new ArrayList<>();
+        final ArrayList<String> studyEndTimes = new ArrayList<>();
+        final Cursor c = db.getAllStudies();
+
+        while (c.moveToNext()){
+            Cursor subject = db.getSingleSubject(c.getInt(2));  // Get corresponding subject
+            subject.moveToFirst();
+            String subjectName = subject.getString(1);  // Get subject name
+
+            studyIds.add(c.getInt(0));
+            studyTitles.add(c.getString(1));
+            subjectNames.add(subjectName);
+            studyColours.add(c.getInt(3));
+            studyDates.add(c.getString(4));
+            studyStartTimes.add(c.getString(5));
+            studyEndTimes.add(c.getString(6));
+        }
+
+        RecyclerView studyRecyclerView = view.findViewById(R.id.study_recycler_view);
+        final StudyRecyclerViewAdapter studyAdapter = new StudyRecyclerViewAdapter(studyIds, studyTitles,
+                subjectNames, studyColours, studyDates, studyStartTimes, studyEndTimes, getActivity());
+        studyRecyclerView.setAdapter(studyAdapter);
+        studyRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/timetable/Study/StudyRecyclerViewAdapter.java
@@ -0,0 +1,114 @@
+package com.example.timetable.Study;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.timetable.R;
+
+import java.util.ArrayList;
+
+public class StudyRecyclerViewAdapter extends RecyclerView.Adapter<StudyRecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "Study Recycler View";
+    private ArrayList<Integer> studyIds;
+    private ArrayList<String> studyTitles;
+    private ArrayList<String> subjectNames;
+    private ArrayList<Integer> studyColours;
+    private ArrayList<String> studyDates;
+    private ArrayList<String> studyStartTimes;
+    private ArrayList<String> studyEndTimes;
+    private Context context;
+
+    public StudyRecyclerViewAdapter(ArrayList<Integer> studyIds, ArrayList<String> studyTitles,
+                                    ArrayList<String> subjectNames, ArrayList<Integer> studyColours,
+                                    ArrayList<String> studyDates, ArrayList<String> studyStartTimes,
+                                    ArrayList<String> studyEndTimes, Context context) {
+        this.studyIds = studyIds;
+        this.studyTitles = studyTitles;
+        this.subjectNames = subjectNames;
+        this.studyColours = studyColours;
+        this.studyDates = studyDates;
+        this.studyStartTimes = studyStartTimes;
+        this.studyEndTimes = studyEndTimes;
+        this.context = context;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView studyTitle, subjectName, studyColour, studyDate, studyStartTime, studyEndTime;
+        FrameLayout studyLayout;
+        CardView studyCard;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            studyTitle = itemView.findViewById(R.id.study_title);
+            subjectName = itemView.findViewById(R.id.subject_name);
+            studyColour = itemView.findViewById(R.id.study_colour);
+            studyDate = itemView.findViewById(R.id.study_date);
+            studyStartTime = itemView.findViewById(R.id.study_start);
+            studyEndTime = itemView.findViewById(R.id.study_end);
+            studyLayout = itemView.findViewById(R.id.study_layout);
+            studyCard = itemView.findViewById(R.id.study_card);
+        }
+    }
+
+    @NonNull
+    @Override
+    public StudyRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.study_list, parent,false);
+        return new ViewHolder(view);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.studyTitle.setText(studyTitles.get(position));
+        holder.subjectName.setText(subjectNames.get(position));
+        holder.studyColour.setBackgroundColor(studyColours.get(position));
+        holder.studyDate.setText(studyDates.get(position));
+        holder.studyStartTime.setText(studyStartTimes.get(position));
+        holder.studyEndTime.setText(studyEndTimes.get(position));
+
+//        holder.studyCard.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int studyId = studyIds.get(position);
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("id", studyId);
+//
+//                EditStudyFragment editStudyFragment = new EditStudyFragment();
+//                editStudyFragment.setArguments(bundle);
+//                AppCompatActivity activity = (AppCompatActivity) view.getContext();
+//
+//                activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        editStudyFragment).addToBackStack(null).commit();
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return studyIds.size();
+    }
+
+    public void removeItem(int position) {
+        studyIds.remove(position);
+        studyTitles.remove(position);
+        subjectNames.remove(position);
+        studyColours.remove(position);
+        studyDates.remove(position);
+        studyStartTimes.remove(position);
+        studyEndTimes.remove(position);
+
+        notifyItemRemoved(position);
+    }
+}
